255423d7fc880ef044052adfa8696c516e4a2cbd
antongolubev5 <24102002Aa>
2020-03-08 19:36:51 +0300

    upd 08.03.20

--- parsing_corpus.py
+++ parsing_corpus.py
@@ -112,13 +112,32 @@ def creating_entities_vocab(directory_path, files: list):
     return entities_with_sentiments
 
 
-def searching_contexts_by_entities():
+def searching_contexts_by_entities(directory_path, corpus_name):
     """
-    попытка выделения персональных сущностей из имеющейся коллекции
+    по имеющимся сущностям набираем из корпуса выборку контекстов
+    :param directory_path:
+    :param corpus_name:
     :return:
     """
 
-    pass
+    contexts_for_entities = open(os.path.join(directory_path, 'contexts_for_labeled_entities'), 'w')
+    entities_vocab = creating_entities_vocab(directory_path, ['nouns_person_neg'])
+
+    # пробегаем по ВСЕМ текстам корпуса и выискиваем предложения, содержащие размеченные слова из словаря
+    # пока просто принтим
+    for month in os.listdir(os.path.join(directory_path, corpus_name)):
+        for day in os.listdir(os.path.join(directory_path, corpus_name, month)):
+            for utf in os.listdir(os.path.join(directory_path, corpus_name, month, day)):
+                for item in os.listdir(os.path.join(directory_path, corpus_name, month, day, utf, 'items')):
+                    tree = ET.parse(
+                        os.path.join(os.path.join(directory_path, corpus_name, month, day, utf, 'items', item)))
+                    text = tree.getroot()[0].text
+                    print(text)
+
+                for text in os.listdir(os.path.join(directory_path, corpus_name, month, day, utf, 'texts')):
+                    f = open(os.path.join(directory_path, corpus_name, month, day, utf, 'texts', text), 'r')
+                    list2vertical(text2sentences(f.read()))
+                    f.close()
 
 
 def searching_personal_entities(directory_path, file_from, file_to):
@@ -130,7 +149,6 @@ def searching_personal_entities(directory_path, file_from, file_to):
     :return:
     """
 
-    # проходим по строкам, если слово не нужно, то удаляем строку
     file_from = open(os.path.join(directory_path, file_from), 'r+')
     file_to = open(os.path.join(directory_path, file_to), 'w')
 
@@ -140,14 +158,42 @@ def searching_personal_entities(directory_path, file_from, file_to):
             file_to.write(line)
 
 
+def text2sentences(text):
+    """
+    разделение текста на предложения
+    """
+
+    nlp = spacy.load('/media/anton/ssd2/data/datasets/spacy-ru/ru2')
+    nlp.add_pipe(nlp.create_pipe('sentencizer'), first=True)
+    doc = nlp(text)
+    sentences = [sent.string.strip() for sent in doc.sents]
+
+    return sentences
+
+
+def list2vertical(lst):
+    """
+    вертикальная печать списка предложений
+    """
+
+    for element in lst:
+        print(element)
+
+
 def main():
     """
-    нужно из словаря тональных существительных вытащить те, которыы описывают людей
+    нужно из словаря тональных существительных вытащить те, которые описывают людей
     и набрать выборку - если встретилось подобное слово - то контекст негативный
     """
 
     directory_path = '/media/anton/ssd2/data/datasets/aspect-based-sentiment-analysis'
     corpus_name = 'Rambler_source_test3'
+
+    f = open(
+        '/media/anton/ssd2/data/datasets/aspect-based-sentiment-analysis/Rambler_source_test3/201101/20110101/20110101000749_utf/texts/22655843.txt',
+        'r')
+    text = f.read()
+    print(text2sentences(text))
 
     # пробуем вытащить персональные сущ
     # searching_personal_entities(directory_path, 'nouns_neg', 'nouns_person_neg')
@@ -159,10 +205,13 @@ def main():
     # mkdir_labeled_texts(directory_path, corpus_name, 'labeled_items')
     # searching_entities_in_corpus(directory_path, corpus_name, entities_with_sentiments)
 
-    text = 'инженер-программист Владимир Путин устроил самый настоящий разнос кое-какому губернатору Московской области Борису Громову'
-    tokens = spacy_tokenizer(text, True)
-    print(tokens)
-    print([token for token in tokens if token in list(entities_with_sentiments.keys())])
+    searching_contexts_by_entities(directory_path, corpus_name)
+
+    print(0)
+    # text = 'инженер-программист Владимир Путин устроил самый настоящий разнос кое-какому губернатору Московской области Борису Громову'
+    # tokens = spacy_tokenizer(text, True)
+    # print(tokens)
+    # print([token for token in tokens if token in list(entities_with_sentiments.keys())])
 
 
 if __name__ == '__main__':
