todo: fix regexp masking & divide sentences with several entities

search_thematic_contexts.py
@@ -40,7 +40,7 @@ def search_thematic_contexts(banks_or_operators='banks'):
 
     with open(os.path.join('/home/anton/data/ABSA/contexts/txt', 'contexts_for_labeled_entities_2'),
               'r') as corpus_sentences:
-        firstNlines = corpus_sentences.readlines()[1750000:2000000]
+        firstNlines = corpus_sentences.readlines()[2900000:3300000]
 
     nlp = spacy.load('/home/anton/PycharmProjects/spacy-ru/ru2e')
     ner_model = build_model(configs.ner.ner_rus_bert, download=True)
@@ -60,7 +60,7 @@ def search_thematic_contexts(banks_or_operators='banks'):
         except RuntimeError:
             continue
 
-    with open('neutral_telecom_contexts_5.txt', 'w') as f:
+    with open('neutral_telecom_contexts_9.txt', 'w') as f:
         for line in contexts:
             f.write(line + '\n')
 
@@ -76,9 +76,39 @@ def txt2csv(file_from, file_to):
     df.to_csv(file_to, index=False, sep='\t')
 
 
-def clean_contexts(file_name, tokenization=False, tf_idf=False, find_entity=False, mask_entity=False):
+def clean_banks_contexts(file_name, tokenization=False, tf_idf=False, find_entity=False, mask_entity=False):
     """
-    чистка набора предложений и его подготовка к использованию в BERT
+    чистка банковских контекстов и их подготовка к подаче в BERT
+    """
+    contexts = pd.read_csv(file_name, sep='\t')
+    if tokenization:
+        nlp = spacy.load('/home/anton/PycharmProjects/spacy-ru/ru2e')
+        contexts['text_tok'] = contexts['text'].apply(lambda x: ' '.join(spacy_tokenizer(x, False, nlp)))
+
+    if tf_idf:
+        corpus = list(contexts['text_tok'])
+        vectorizer = TfidfVectorizer(min_df=0.002, use_idf=True, ngram_range=(1, 1))
+        X = vectorizer.fit_transform(corpus)
+        X = cosine_similarity(X)
+        pairs = np.argwhere(X > 0.8).T
+        diag = pairs[0] != pairs[1]
+        pairs = pairs.T[diag]
+        numbers = np.unique(np.max(pairs, axis=1))
+        contexts = contexts.drop(index=contexts.iloc[numbers].index)
+
+    if find_entity:
+        contexts['has_bank'] = contexts['text_tok'].apply(lambda x: x.count('банк'))
+        contexts = contexts[contexts['has_bank'] == 1]
+
+    if mask_entity:
+        contexts['text_tok'] = contexts['text_tok'].apply(lambda x: re.sub('\s*\S*банк\S*\s*', ' MASK ', x))
+
+    contexts.to_csv(file_name[:-4] + '_cleaned.csv', index=False, sep='\t')
+
+
+def clean_telecom_contexts(file_name, tokenization=False, tf_idf=False, find_entity=False, mask_entity=False):
+    """
+    чистка банковских контекстов и их подготовка к подаче в BERT
     """
     contexts = pd.read_csv(file_name, sep='\t')
     if tokenization:
@@ -108,8 +138,11 @@ def clean_contexts(file_name, tokenization=False, tf_idf=False, find_entity=Fals
 
 def main():
     # search_thematic_contexts(banks_or_operators='telecom')
-    # txt2csv('neutral_telecom_contexts_5.txt', 'neutral_telecom_contexts_5.csv')
-    clean_contexts('neutral_telecom_contexts.csv', tokenization=True, tf_idf=True, find_entity=True, mask_entity=True)
+    # txt2csv('neutral_telecom_contexts_9.txt', 'neutral_telecom_contexts_9.csv')
+    # clean_banks_contexts('neutral_banks_contexts.csv', tokenization=True, tf_idf=True, find_entity=True,
+    #                     mask_entity=True)
+    clean_telecom_contexts('neutral_telecom_contexts.csv', tokenization=False, tf_idf=False, find_entity=False,
+                           mask_entity=False)
 
 
 if __name__ == '__main__':